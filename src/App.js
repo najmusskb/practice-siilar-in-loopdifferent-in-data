01db55ba846bae1ea5bc14b1c53893c34fb52633
najmusskb <najmussakib173@gmail.com>
2022-09-27 12:18:31 +0600

    data load

--- src/App.js
+++ src/App.js
@@ -43,11 +43,12 @@ export default App;
 
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadPost></LoadPost>
       <Distric name="Nowakhali" Special="vibag"></Distric>
       <Distric name="B-Bariya" Special="war" ></Distric>
       <Distric name="Comilla" Special='moynamoti' ></Distric>
@@ -56,6 +57,42 @@ function App() {
   );
 }
 
+
+
+//state -05  for data load-----------------------------------------
+function LoadPost(props){
+  const [posts, setPost]=useState([]); //step-1
+
+  useEffect(()=>{
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>res.json())
+    // .then(data=>console.log(data))
+    .then(data =>setPost(data))
+  },[]) // second step-2
+
+  return(
+    <div>
+      <h1>Post:{posts.length} </h1>
+
+      {
+        posts.map(post=><Post title={post.title} body={post.body}></Post>) //set atribute
+      }
+    </div>
+  )
+}
+function Post(props){
+
+  return(
+
+    <div className='post'>
+      <h2>Title: {props.title} </h2>
+      <p>Body :{props.body}</p>
+    </div>
+  )
+}
+
+// ---------------------------------------------------------------------------------------------
 
 function Distric(props){
 
@@ -89,6 +126,8 @@ padding:'20px'
   )
   
 }
+
+
 
 export default App;
 
