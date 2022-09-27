819714feba827212ba3cee3eea34c52beab857e7
najmusskb <najmussakib173@gmail.com>
2022-09-27 11:32:53 +0600

    use state,event handler ,ui te dekhano , button er sathe eventhandler add kora

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import logo from './logo.svg';
+// similar in look ,,different in data
+
+// 1.-----------------------------------------------------------------------------------------------
+
+/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -16,7 +20,7 @@ function App() {
 function Distric(props){
 
 const districSyle={
-backgroundColor:'yellow',
+backgroundColor:'green',
 margin:'200px',
 borderRadious:'30px'
 }
@@ -31,5 +35,61 @@ borderRadious:'30px'
 
 export default App;
 
+ */
+//2 -----------------------------------------------------------------------------------------------------------
 
-// similar in look ,,different in data
+// user kichu ta increass korbe 
+
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Distric name="Nowakhali" Special="vibag"></Distric>
+      <Distric name="B-Bariya" Special="war" ></Distric>
+      <Distric name="Comilla" Special='moynamoti' ></Distric>
+    </div>
+    
+  );
+}
+
+
+function Distric(props){
+
+  // state 1 (state declare kora)
+const [power, setPower]=useState(1);
+
+// state -03 (event handlaer set)
+const boostPower =()=> {
+  const newPower=power*2;
+  setPower(newPower);
+
+}
+
+const districSyle={
+backgroundColor:'skyblue',
+margin:'300px',
+borderRadious:'30px',
+padding:'20px'
+}
+
+// state-02(ui te dekhano)
+  return(
+    <div style={districSyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty: {props.Special}</p>
+      <p>Power: {power} </p>
+
+    {/* state-04  (button er sathe event handler ke add kore fela) */}
+      <button onClick={boostPower}>Boost The Power : </button>
+    </div>
+  )
+  
+}
+
+export default App;
+
+
